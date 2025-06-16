f92a23304dfabcbd967d916804f8eef1f0b16062
r.roda <r.roda@outsource.com>
2025-06-16 22:57:52 +0800

    Final update for todays vlog

--- app/src/main/java/com/example/projectkape/Login.kt
+++
@@ -1,7 +0,0 @@
-package com.example.projectkape
-
-import android.widget.Button
-import android.widget.TextView
-
-private lateinit var loginButton: Button
-private lateinit var signupButton: TextView
