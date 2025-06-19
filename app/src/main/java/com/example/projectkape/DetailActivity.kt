aef00dc2c5e2a3487a6f6adfe2cd3d97165170a3
jiaanortillano <jiaan.ortillano@edufied.edu.vn>
2025-06-19 21:26:49 +0800

    Update and Additonal UI

---
+++ app/src/main/java/com/example/projectkape/DetailActivity.kt
@@ -0,0 +1,15 @@
+package com.example.digikulturas
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectkape.R
+
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
